a9f0bd2db8f641c713676240ec9381c871bed0bb
rajvishah <rajvi.a.shah@gmail.com>
2020-10-01 21:39:34 -0700

    update

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1lbKcaKlHUKlWf-9-YjSuULKIy5cvZwJGYj97Ab4YpHo/edit?usp=sharing';
 
 function init() {
-	Tabletop.init( {
-		key: publicSpreadsheetUrl,
-		callback: showInfo,
-		simpleSheet: true 
-	} )
+	Papa.parse(publicSpreadsheetUrl, {
+		download: true,
+		header: true,
+		complete: showInfo
+	})
 }
-
 	
-function showInfo(data, tabletop) {
-data.forEach(function(data) {
+function showInfo(results) {
+	var data = results.data
+	alert('Successfully processed ' + data.length + ' rows!')
+
+	data.forEach(function(data) {
 body.innerHTML = data.body;
 body2.innerHTML = data.body2;
 });
